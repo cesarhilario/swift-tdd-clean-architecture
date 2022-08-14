533d2449cd43a717470985e8155b55dadb4c0f1f
César Hilário <cesar.hilario.dev@gmail.com>
2022-08-14 17:38:50 -0300

    refactor: move AddAccountModel creation to a factory helper method

--- DataTests/RemoteAddAccountTests.swift
+++ DataTests/RemoteAddAccountTests.swift
@@ -33,15 +33,14 @@ class RemoteAddAccountTests: XCTestCase {
         let url = URL(string: "http://any-url.com")!;
         let httpClientSpy = HttpClientSpy();
         let sut = RemoteAddAccount(url: url, httpClient: httpClientSpy);
-        let addAccountModel = AddAccountModel(name: "any_name", email: "any_email@emai.com", password: "any_password", passwordConfirmation: "any_password");
-        sut.add(addAccountModel: addAccountModel);
+        sut.add(addAccountModel: makeAccountModel());
         XCTAssertEqual(httpClientSpy.url, url);
     }
     
     func test_add_should_call_httpClient_with_correct_data() {
         let httpClientSpy = HttpClientSpy();
         let sut = RemoteAddAccount(url: URL(string: "http://any-url.com")!, httpClient: httpClientSpy);
-        let addAccountModel = AddAccountModel(name: "any_name", email: "any_email@emai.com", password: "any_password", passwordConfirmation: "any_password");
+        let addAccountModel = makeAccountModel();
         sut.add(addAccountModel: addAccountModel);
         let data = try? JSONEncoder().encode(addAccountModel);
         XCTAssertEqual(httpClientSpy.data, data);
@@ -50,6 +49,11 @@ class RemoteAddAccountTests: XCTestCase {
 
 // Helpers
 extension RemoteAddAccountTests {
+    // Factory Design Pattern
+    func makeAccountModel() -> AddAccountModel {
+        return AddAccountModel(name: "any_name", email: "any_email@emai.com", password: "any_password", passwordConfirmation: "any_password");
+    }
+    
     class HttpClientSpy: HttpPostClient {
         var url: URL?;
         var data: Data?;
