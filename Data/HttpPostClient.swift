0914e0b46f94fa2b6b0b53007aa266f123901ea8
César Hilário <cesar.hilario.dev@gmail.com>
2022-08-14 18:03:41 -0300

    refactor: move production code to their own files

---
+++ Data/HttpPostClient.swift
@@ -0,0 +1,12 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Cesar Hilario on 14/08/22.
+//
+
+import Foundation
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?);
+}
---
+++ Data/RemoteAddAccount.swift
@@ -0,0 +1,24 @@
+//
+//  RemoteAddAccount.swift
+//  Data
+//
+//  Created by Cesar Hilario on 14/08/22.
+//
+
+import Foundation
+import Domain
+
+public final class RemoteAddAccount {
+    private let url: URL;
+    
+    private let httpClient: HttpPostClient;
+    
+    public init(url: URL, httpClient: HttpPostClient) {
+        self.url = url;
+        self.httpClient = httpClient;
+    }
+    
+    public func add(addAccountModel: AddAccountModel) {
+        httpClient.post(to: url, with: addAccountModel.toData());
+    }
+}
