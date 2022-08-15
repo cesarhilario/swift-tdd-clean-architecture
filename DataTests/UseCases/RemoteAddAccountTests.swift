7c13b242a8b30271b888aaf3c2e481da0e5db0d1
César Hilário <cesar.hilario.dev@gmail.com>
2022-08-14 22:34:39 -0300

    chore: add assert file and line in the expect helper method

--- DataTests/UseCases/RemoteAddAccountTests.swift
+++ DataTests/UseCases/RemoteAddAccountTests.swift
@@ -73,12 +73,12 @@ extension RemoteAddAccountTests {
         return URL(string: "http://any-url.com")!;
     }
 
-    func expect(_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel, DomainError>, when action: () -> Void) {
+    func expect(_ sut: RemoteAddAccount, completeWith expectedResult: Result<AccountModel, DomainError>, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
         let exp = expectation(description: "waiting");
         sut.add(addAccountModel: makeAddAccountModel()) { receivedResult in
             switch (expectedResult, receivedResult) {
-            case(.failure(let expectedError), .failure(let receivedError)): XCTAssertEqual(expectedError, receivedError);
-            case(.success(let expectedAccount), .success(let receivedAccount)): XCTAssertEqual(expectedAccount, receivedAccount);
+            case(.failure(let expectedError), .failure(let receivedError)): XCTAssertEqual(expectedError, receivedError, file: file, line: line);
+            case(.success(let expectedAccount), .success(let receivedAccount)): XCTAssertEqual(expectedAccount, receivedAccount, file: file, line: line);
             default: XCTFail("Expected \(expectedResult) received \(receivedResult) insted");
             }
 
