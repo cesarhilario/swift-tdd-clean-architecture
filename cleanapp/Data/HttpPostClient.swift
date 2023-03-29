c3a01b7076f36d6fd38c7a98dac70cf73772d730
César Hilário <cesar.hilario.dev@gmail.com>
2023-03-28 23:17:59 -0300

    refactor: move production code to their own files

---
+++ cleanapp/Data/HttpPostClient.swift
@@ -0,0 +1,13 @@
+//
+//  HttpPostClient.swift
+//  Data
+//
+//  Created by Cesar Hilario on 28/03/23.
+//
+
+import Foundation
+
+
+public protocol HttpPostClient {
+    func post(to url: URL, with data: Data?);
+}
