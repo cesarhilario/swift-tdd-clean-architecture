24e9d6fb36b17fac978eeca58a86eb72b8796e27
César Hilário <cesar.hilario.dev@gmail.com>
2023-03-28 21:46:38 -0300

    chore: AddAccount use case

---
+++ cleanapp/Domain/AccountModel.swift
@@ -0,0 +1,16 @@
+//
+//  AccountModel.swift
+//  Domain
+//
+//  Created by Cesar Hilario on 28/03/23.
+//
+
+import Foundation
+
+struct AccountModel {
+    var id: String;
+    var name: String;
+    var email: String;
+    var password: String;
+
+}
